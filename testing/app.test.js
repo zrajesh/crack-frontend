614bab365b72abf26c18f72446e03ad90745c755
zrajesh <hirajesh.official@gmail.com>
2025-03-09 11:40:31 +0530

    Testing: Add unit testing

---
+++ testing/app.test.js
@@ -0,0 +1,15 @@
+// Unit Testing
+
+const sortingByAge = require("./app");
+
+test("testing if the first user is Elon after sorting", () => {
+  const sortedData = sortingByAge();
+
+  expect(sortedData[0].name).toBe("Elon");
+});
+
+test("testing if the sorted data has length of 4", () => {
+  const sortedData = sortingByAge();
+
+  expect(sortedData).toHaveLength(4);
+});
